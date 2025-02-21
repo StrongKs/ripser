ae8ae84d076a9ee311b6ae9bc682be73bea7de81
StringKs <kent.phipps@ufl.edu>
2025-02-20 19:48:41 -0500

    alice sandbox. ripserSol.cpp is the one file that should be running correctly.

--- ripserR/src/main.cpp
+++ ripserR/src/main.cpp
@@ -2,11 +2,13 @@
 // Created by Kent Phipps on 2/16/25.
 //
 
+#pragma once
+
 #include <iostream>
 #include <vector>
-#include "ripser.h"
-#include "resource/sparse_distance_matrix.h"
-#include "resource/compressed_distance_matrix.h"
+#include "ripserSol.cpp"
+//#include "resource/sparse_distance_matrix.h"
+//#include "resource/compressed_distance_matrix.h"
 
 using namespace std;
 
@@ -20,22 +22,28 @@ int main() {
             {2.0, 1.5, 0}
     };
 
+    vector<value_t> dis_vector = {4.0f,
+                                  3.0f, 5.0f,
+                                  5.0f, 3.0f, 4.0f};
+
     // Convert to a compressed lower distance matrix
-    compressed_lower_distance_matrix comp_dist(distance_matrix);
+    compressed_lower_distance_matrix(std::move(dis_vector));
+    cout << "hello" << endl;
+//    compressed_lower_distance_matrix comp_dist(dis_vector);
 
-    // Instantiate and test Ripser with compressed lower distance matrix
-    ripser<compressed_lower_distance_matrix> ripser_comp(std::move(comp_dist), 2, 2.0, 1.0, 2);
-    cout << "Computing barcodes for compressed lower distance matrix..." << endl;
-    ripser_comp.compute_barcodes();
-
-    // Convert to a sparse distance matrix
-    sparse_distance_matrix sparse_dist(distance_matrix, 2.0);
-
-    // Instantiate and test Ripser with sparse distance matrix
-    ripser<sparse_distance_matrix> ripser_sparse(std::move(sparse_dist), 2, 2.0, 1.0, 2);
-    cout << "Computing barcodes for sparse distance matrix..." << endl;
-    ripser_sparse.compute_barcodes();
-
-    cout << "Testing completed!" << endl;
+//    // Instantiate and test Ripser with compressed lower distance matrix
+//    ripser<compressed_lower_distance_matrix> ripser_comp(std::move(comp_dist), 2, 2.0, 1.0, 2);
+//    cout << "Computing barcodes for compressed lower distance matrix..." << endl;
+//    ripser_comp.compute_barcodes();
+//
+//    // Convert to a sparse distance matrix
+//    sparse_distance_matrix sparse_dist(distance_matrix, 2.0);
+//
+//    // Instantiate and test Ripser with sparse distance matrix
+//    ripser<sparse_distance_matrix> ripser_sparse(std::move(sparse_dist), 2, 2.0, 1.0, 2);
+//    cout << "Computing barcodes for sparse distance matrix..." << endl;
+//    ripser_sparse.compute_barcodes();
+//
+//    cout << "Testing completed!" << endl;
     return 0;
 }
